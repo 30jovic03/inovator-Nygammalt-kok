473711cf37a1c197cc7101f8732c58b4f6ceebd4
Mladen Jovic <30jovic03@gmail.com>
2021-03-24 23:10:41 +0100

    Implement home section

--- assets/js/toggleMenu.js
+++ assets/js/toggleMenu.js
@@ -2,7 +2,7 @@ function toggleMenu() {
   var nav = document.getElementById("main-navbar");
   if (nav.className === "navbar") {
     nav.classList.add('show-menu');
-    nav.style.height = '90vh';
+    nav.style.height = '100vh';
   } else {
     nav.classList.remove('show-menu');
     nav.style.height = 0;
