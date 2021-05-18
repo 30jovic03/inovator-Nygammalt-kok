2760fbbc95d8cc2c319680296b958af5637c94c0
Mladen Jovic <30jovic03@gmail.com>
2021-05-19 00:10:03 +0200

    bootstrap in products and category page

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,16 +1,17 @@
-$('.carousel[data-type="multi"] .item').each(function(){
-    var next = $(this).next();
-    if (!next.length) {
-      next = $(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
+$('.carousel .carousel-item').each(function(){
+  var minPerSlide = 4;
+  var next = $(this).next();
+  if (!next.length) {
+  next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
   
-    for (var i=0;i<4;i++) {
+  for (var i=0;i<minPerSlide;i++) {
       next=next.next();
       if (!next.length) {
-          next = $(this).siblings(':first');
-        }
+        next = $(this).siblings(':first');
+      }
       
       next.children(':first-child').clone().appendTo($(this));
     }
-  });
+});
