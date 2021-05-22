b643ef7f87e77c1c302ffd755f92df2571136fab
Mladen Jovic <30jovic03@gmail.com>
2021-05-22 10:57:04 +0200

    New carousel

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,17 +1,21 @@
-$('.carousel .carousel-item').each(function(){
-  var minPerSlide = 4;
-  var next = $(this).next();
-  if (!next.length) {
-  next = $(this).siblings(':first');
-  }
-  next.children(':first-child').clone().appendTo($(this));
+
+$('#my-carousel').on('slide.bs.carousel', function (e) {
   
-  for (var i=0;i<minPerSlide;i++) {
-      next=next.next();
-      if (!next.length) {
-        next = $(this).siblings(':first');
+  var $e = $(e.relatedTarget);
+  var idx = $e.index();
+  var itemsPerSlide = 5;
+  var totalItems = $('.carousel-item').length;
+
+  if (idx >= totalItems-(itemsPerSlide-1)) {
+      var it = itemsPerSlide - (totalItems - idx);
+      for (var i=0; i<it; i++) {
+          // append slides to end
+          if (e.direction=="left") {
+              $('.carousel-item').eq(i).appendTo('.carousel-inner');
+          }
+          else {
+              $('.carousel-item').eq(0).appendTo('.carousel-inner');
+          }
       }
-      
-      next.children(':first-child').clone().appendTo($(this));
-    }
+  }
 });
