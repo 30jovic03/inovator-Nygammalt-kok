b5dc92f5cb5b6c5bdc80bd0a888e744fce31a426
Mladen Jovic <30jovic03@gmail.com>
2021-06-09 22:19:13 +0200

    Scroll-up button & hover on 2 nav buttons

--- assets/js/changeHeaderColor.js
+++ assets/js/changeHeaderColor.js
@@ -1,10 +1,13 @@
 window.onscroll = () => {scrollFunction()};
 
 function scrollFunction() {
-  var header = document.getElementById("header");
+  let header = document.getElementById("header");
+  let scrollButton = document.getElementById("scroll-up-button");
   if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
     header.classList.add('header-color');
+    scrollButton.style.display = 'block';
   } else {
     header.classList.remove('header-color');
+    scrollButton.style.display = 'none';
   }
 }
